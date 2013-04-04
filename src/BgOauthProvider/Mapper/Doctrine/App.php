2ba7df2cdd2d6bc16ff0aed7f386c631afc52f00
Brian Gallagher <gallagher.brian@gmail.com>
2013-04-04 16:23:26 +0100

    forgot app mapper

---
+++ src/BgOauthProvider/Mapper/Doctrine/App.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BgOauthProvider\Mapper\Doctrine;
+
+use BgOauthProvider\Mapper\AppInterface;
+use BgOauthProvider\Entity\App as AppEntity;
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Persistence\ObjectManagerAwareInterface;
+
+class App implements AppInterface
+{
+
+    /**
+     * @var ObjectManager
+     */
+    protected $objectManager;
+
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * @param string $consumerKey
+     * @return AppEntity
+     */
+    public function findAppByConsumerKey($consumerKey)
+    {
+        return $this->getObjectManager()->getRepository('BgOauthProvider\Entity\App')
+            ->findOneBy(array('consumer_key' => $consumerKey));
+    }
+
+    /**
+     * Set the object manager
+     *
+     * @param ObjectManager $objectManager
+     */
+    public function setObjectManager(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * Get the object manager
+     *
+     * @return ObjectManager
+     */
+    public function getObjectManager()
+    {
+        return $this->objectManager;
+    }
+
+}
